9238fd944853c93520fd226064cabbf8c6bdbadd
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-16 00:03:28 +0900

    Title: 오름 내림차순 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240116/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, arr[110];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i<n;i++){
+        cin >> arr[i];
+    }
+    sort(arr,arr+n);
+    for (int i =0;i<n; i++){
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
+    sort(arr,arr+n,greater<int>());
+    for (int i =0;i<n; i++){
+        cout << arr[i] << ' ';
+    }
+    return 0;
+}
