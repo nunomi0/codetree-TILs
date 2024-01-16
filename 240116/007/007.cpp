f1663aba4ac4f8366b0c7dc9e237efd2b37b0785
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-16 20:39:17 +0900

    Title: 007, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240116/007/007.cpp
+++ 240116/007/007.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-pair<string,pair<string,int>> m;
-string a,b;
-int c;
+// class 이용
+string s_code;
+char m_point;
+int t_time;
+
+class spy{
+    public:
+        string secret_code;
+        char meeting_point;
+        int time;
+        spy(string secret_code, char meeting_point, int time){
+            this->secret_code = secret_code;
+            this->meeting_point = meeting_point;
+            this->time= time;
+        }
+};
 
 
 int main() {
-    cin >> m.first >> m.second.first >> m.second.second;
-    cout << "secret code : " << m.first << '\n';
-    cout << "meeting point : " << m.second.first << '\n';
-    cout << "time : " << m.second.second << '\n';
+    cin >> s_code >> m_point >> t_time;
+    spy s = spy(s_code, m_point, t_time);
+    cout << "secret code : " << s.secret_code << endl;
+    cout << "meeting point : " << s.meeting_point << endl;
+    cout << "time : " << s.time << endl;
 
     return 0;
 }
