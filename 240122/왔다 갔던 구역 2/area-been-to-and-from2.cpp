e8a108a2cbfc78694ef0b8c893943ecd83d39a80
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-22 18:29:11 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240122/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240122/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -2,30 +2,19 @@
 using namespace std;
 
 
-int n,x,arr[2010], idx=1000;
+int n,x,arr[2010],idx=1000,d;
 char s;
 
 int main() {
     cin >> n;
-    arr[idx]++;
     for (int i = 0; i<n; i++){
         cin >> x >> s;
-        if (s=='R'){
-            for (int j = 0; j<x; j++){
-                idx++;
-                arr[idx]++;
-            }
-        }
-        else {
-            for (int j = 0; j<x; j++){
-                idx--;
-                arr[idx]++;
-            }
-        }
+        if (s=='R') for (int j = 0; j<x; j++) arr[++idx]++;
+        else for (int j = 0; j<x; j++) arr[idx--]++;
     }
 
     int ans=0;
-    for (int i = 0; i<=1000; i++) if (arr[i]>=2) ans++;
+    for (int i = 0; i<=2000; i++) if (arr[i]>=2) ans++;
     cout << ans;
 
     return 0;
