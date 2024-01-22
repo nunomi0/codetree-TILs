07cffba0945d73a5fac9bcb9e8a1eae412631df9
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-22 14:03:51 +0900

    Title: 줄 세우기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240122/줄 세우기 2/line-up-students-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n,h,w;
+pair<pair<int,int>,int> s[1010]; // h w i
+
+bool cmp(pair<pair<int,int>,int> a, pair<pair<int,int>,int> b){
+    if (a.first.second!=b.first.second) return a.first.second<b.first.second;
+    return a.first.second>b.first.second;
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i<n; i++){
+        cin >> h >> w;
+        s[i]={{h,w},i+1};
+    }
+    sort(s,s+n,cmp);
+    for (int i = 0; i<n; i++){
+        cout << s[i].first.first << ' ' << s[i].first.second << ' ' << s[i].second << '\n';
+    }
+    return 0;
+}
