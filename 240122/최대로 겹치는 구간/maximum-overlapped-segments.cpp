9981589731ae861a082af66a97ab3cc54e7c860c
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-22 17:54:51 +0900

    Title: 최대로 겹치는 구간, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240122/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 240122/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int n,x1,x2,arr[110];
+int n,x1,x2,arr[210];
 
 int main() {
     cin >> n;
     for (int i = 0; i<n; i++){
         cin >> x1 >> x2;
-        for (int j = x1; j<x2; j++){
+        for (int j = x1+100; j<x2+100; j++){
             arr[j]++;
         }
     }
     int ans=0;
-    for (int i = 0; i<n; i++){
+    for (int i = 0; i<=200; i++){
         ans=max(ans,arr[i]);
     }
     cout << ans;
