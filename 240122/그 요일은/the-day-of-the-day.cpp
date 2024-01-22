5821b7058f53298fe3979efa89c74d8b70c867b2
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-22 16:59:27 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240122/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+int m1,m2,d1,d2;
+int months[20]={0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+string days[7]={"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+string day;
+
+int main() {
+    cin >> m1 >> d1 >> m2 >> d2 >> day;
+
+    int cnt=0;
+    for (int i = m1; i<m2; i++){
+        cnt+=months[i];
+    }
+    cnt+=-d1+d2;
+
+    for (int i = 0; i<7; i++){
+        if (day==days[i]) {
+            cnt+=i;
+            break;
+        }
+    }
+
+    cout << cnt/7;
+    
+    return 0;
+}
