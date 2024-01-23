f9b60cacc12b832ad69e64862873d2335f7898c3
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-24 01:53:12 +0900

    Title: 좌우로 움직이는 로봇, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240124/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int n,m,t,a[2000010],b[2000010],at=1,bt=1,ans=0;
+char c;
+map<char,int> d={{'L',-1},{'R',1}};
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i<n; i++){
+        cin >> t >> c;
+        while (t--) a[at++]=a[at-1]+d[c];
+    }
+    for (int i = 0; i<m-n; i++) a[at++]=a[at-1];
+    for (int i = 0; i<m; i++){
+        cin >> t >> c;
+        while (t--) b[bt++]=b[bt-1]+d[c];
+    }
+    for (int i = 0; i<n-m; i++) b[bt++]=b[bt-1];
+
+    for (int i = 1; i<=max(at,bt); i++){
+        if (a[i]==b[i] && a[i-1]!=b[i-1]) ans++;
+    }
+
+    cout << ans;
+
+    return 0;
+}
