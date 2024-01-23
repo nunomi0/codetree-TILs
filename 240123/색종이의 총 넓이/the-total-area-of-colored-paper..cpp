4d25da6944df575a81bd7da092306068e8f35a12
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-23 16:31:02 +0900

    Title: 색종이의 총 넓이, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240123/색종이의 총 넓이/the-total-area-of-colored-paper..cpp
+++ 240123/색종이의 총 넓이/the-total-area-of-colored-paper..cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int n,x,y,arr[310][310],ans=0;
+int n,x,y,arr[3010][3010],ans=0;
 
 
 int main() {
@@ -15,8 +15,8 @@ int main() {
         }
     }
 
-    for (int i = 0; i<=200; i++){
-        for (int j = 0; j<=200; j++){
+    for (int i = 0; i<=300; i++){
+        for (int j = 0; j<=300; j++){
             if (arr[i][j]) ans++;
         }
     }
