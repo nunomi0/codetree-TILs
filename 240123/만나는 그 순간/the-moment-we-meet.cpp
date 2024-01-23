5962c1a28e6f83a116c478ab571e6e94ecb0c110
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-23 19:18:52 +0900

    Title: 만나는 그 순간, Time: 15ms, Memory: 3MB, Status: Passed - CodeTree

--- 240123/만나는 그 순간/the-moment-we-meet.cpp
+++ 240123/만나는 그 순간/the-moment-we-meet.cpp
@@ -2,32 +2,25 @@
 #include <map>
 using namespace std;
 
-int n,m,at[1010],bt[1010],cnt=0,ai=0,bi=0,al=0,bl=0,ans=-1;
-char ad[1010],bd[1010];
+int n,m,t,a[1000010],b[1000010],at=1,bt=1,ans=-1;
+char c;
 map<char,int> d={{'L',-1},{'R',1}};
 
-int main() {
+
+int main(){
     cin >> n >> m;
-    
-    for (int i = 0; i<n; i++) cin >> ad[i] >> at[i];
-    for (int i = 0; i<m; i++) cin >> bd[i] >> bt[i];
+    for (int i = 0; i<n; i++){
+        cin >> c >> t;
+        while (t--) a[at++]=a[at-1]+d[c];
+    }
+    for (int j = 0; j<m; j++){
+        cin >> c >> t;
+        while (t--) b[bt++]=b[bt-1]+d[c];
+    }
 
-    while (true){
-
-        if (ai>n || bi>m) break;
-
-        cnt++;
-
-        at[ai]--;
-        al+=d[ad[ai]];
-        if (at[ai]==0) ai++;
-
-        bt[bi]--;
-        bl+=d[bd[bi]];
-        if (bt[bi]==0) bi++;
-
-        if (al==bl) {
-            ans=cnt;
+    for (int i = 1; i<min(at,bt); i++){
+        if (a[i]==b[i]) {
+            ans=i;
             break;
         }
     }
