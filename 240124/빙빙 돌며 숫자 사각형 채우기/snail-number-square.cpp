53c7fa2b480772afd7baabe08f173396b097c356
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-24 13:47:31 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240124/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
+++ 240124/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int n,m;
 int arr[110][110],d=0,x=0,y=0;
-int dy[4]={1,0,-1,0};
-int dx[4]={0,1,0,-1};
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
 
 
 int main() {
     cin >> n >> m;
     for (int i = 1; i<=n*m; i++){
-        arr[x][y]=i;
+        arr[y][x]=i;
         int nx=x+dx[d];
         int ny=y+dy[d];
-        if (!(0<=nx && nx<m && 0<=ny && ny<n) || arr[nx][ny]){
+        if (!(0<=nx && nx<m && 0<=ny && ny<n) || arr[ny][nx]){
             d=(d+1)%4;
             nx=x+dx[d];
             ny=y+dy[d];
