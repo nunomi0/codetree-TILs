12f66ccae2482649f78067399afbe270c41b6469
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-24 18:11:13 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240124/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int n,arr[110][110],dir=0,dist=1,idx=2;
+int dx[4]={0,-1,0,1};
+int dy[4]={1,0,-1,0};
+
+
+int main() {
+    cin >> n;
+
+    int x=n/2;
+    int y=n/2;
+
+    arr[x][y]=1;
+
+    while (true){
+        for (int i = 0; i<abs(dist); i++){\
+            x+=dx[dir];
+            y+=dy[dir];
+            arr[x][y]=idx++;
+            if (x==n-1 && y==n-1) {
+                for (int i = 0; i<n; i++){
+                    for (int j = 0; j<n; j++){
+                        cout << arr[i][j] << ' ';
+                    }
+                    cout << '\n';
+                }
+                return 0;
+            }
+        }
+        
+        dir=(dir+1)%4;
+
+        if (dist<0) dist=abs(dist)+1;
+        else dist*=-1;
+    }
+
+    return 0;
+}
