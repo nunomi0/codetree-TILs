0667ae28d45316311872870d0f4db2ddc6a6303b
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-24 11:14:56 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240124/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N,K,P,T,t,x,y,virus[110];
+vector<pair<int,pair<int,int>>> handshake;
+
+
+int main() {
+    cin >> N >> K >> P >> T;
+
+    virus[P]=K+1;
+
+    for (int i = 0; i<T; i++){
+        cin >> t >> x >> y;
+        handshake.push_back({t,{x,y}});
+    }
+    sort(handshake.begin(),handshake.end());
+
+    for (int i = 0; i<=handshake.size(); i++){
+        x=handshake[i].second.first;
+        y=handshake[i].second.second;
+        if (virus[x]>1 || virus[y]>1){
+            if (virus[x]) virus[x]=max(virus[x]-1,1);
+            else virus[x]=K+1;
+            
+            if (virus[y]) virus[y]=max(virus[y]-1,1);
+            else virus[y]=K+1;
+        }
+    }
+
+    for (int i = 1; i<=N; i++) cout << (bool)virus[i];
+
+    return 0;
+}
