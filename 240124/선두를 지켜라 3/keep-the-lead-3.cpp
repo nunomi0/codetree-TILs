e4811e6af350203d0b72a22dae36104debd3fedc
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-24 11:28:25 +0900

    Title: 선두를 지켜라 3, Time: 12ms, Memory: 3MB, Status: Passed - CodeTree

---
+++ 240124/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int n,m,v,t,a[1000010],b[1000010],at=1,bt=1,leader=0,ans=0;
+
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i<n; i++){
+        cin >> v >> t;
+        while (t--) a[at++]=a[at-1]+v;
+    }
+    for (int i = 0; i<m; i++){
+        cin >> v >> t;
+        while (t--) b[bt++]=b[bt-1]+v;
+    }
+    for (int i = 0; i<min(at,bt); i++){
+        if (a[i]==b[i]){
+            if (leader!=0) ans++;
+            leader=0;
+        }
+        else if (a[i]>b[i]){
+            if (leader!=1) ans++;
+            leader=1;
+        }
+        else if (a[i]<b[i]){
+            if (leader!=2) ans++;
+            leader=2;
+        }
+    }
+    cout << ans;
+    return 0;
+}
