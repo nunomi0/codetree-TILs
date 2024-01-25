bd2dfb2a74b415fd5305a401ae13e1853f955aef
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-25 13:59:48 +0900

    Title: 트리의 지름, Time: 209ms, Memory: 8MB, Status: Passed - CodeTree

--- 240125/트리의 지름/diameter-of-tree.cpp
+++ 240125/트리의 지름/diameter-of-tree.cpp
@@ -3,12 +3,16 @@
 #include <cstring>
 using namespace std;
 
-int n,a,b,c,visited[100010],dist[100010];
-vector<pair<int,int>> v[100010];
+int n,a,b,c,visited[100010],tmp=0;
+long long mx=0;
+vector<pair<int,long long>> v[100010];
 
-void recur(int cur, int cnt){
+void recur(int cur, long long cnt){
     visited[cur]=1;
-    dist[cur]=cnt;
+    if (cnt>mx){
+        mx=cnt;
+        tmp=cur;
+    }
     for (int i = 0; i<v[cur].size(); i++){
         int nxt=v[cur][i].first;
         int nd=v[cur][i].second;
@@ -25,24 +29,9 @@ int main() {
         v[b].push_back({a,c});
     }
     recur(1,0);
-
-    int mx=0;
-    int idx=0;
-    for (int i = 1; i<=n; i++){
-        if (dist[i]>mx) {
-            mx=dist[i];
-            idx=i;
-        }
-    }
-
     memset(visited,0,sizeof(visited));
-    recur(idx,0);
-    
-    int ans=0;
-    for (int i = 1; i<=n; i++){
-        ans=max(ans,dist[i]);
-    }
-    cout << ans;
+    recur(tmp,0);
+    cout << mx;
 
     return 0;
 }
