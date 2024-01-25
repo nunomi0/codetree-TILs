c7ea229a12f29266eb078ae258363d6ed69b9b56
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-01-25 15:59:18 +0900

    Title: 트리 노드 제거, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/트리 노드 제거/remove-tree-node.cpp
+++ 240125/트리 노드 제거/remove-tree-node.cpp
@@ -8,7 +8,7 @@ vector<int> v[60];
 void traverse(int cur){
     if (cur==x) return;
     visited[cur]=1;
-    if (v[cur].size()==0) ans++;
+    if (v[cur].size()==0 || (v[cur].size()==1 && v[cur][0]==x)) ans++;
     else {
         for (int i = 0; i<v[cur].size(); i++){
             int nxt=v[cur][i];
@@ -26,7 +26,7 @@ int main() {
         else v[x].push_back(i);
     }
     cin >> x;
-    
+
     traverse(r);
     cout << ans;
     return 0;
