88a49314b4a28ce98dd40fc7a31f857ce14c0243
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-07-08 15:44:47 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240708/더 긴 문자열/longer-string.cpp
+++ 240708/더 긴 문자열/longer-string.cpp
@@ -8,7 +8,8 @@ int main() {
 
     cin >> s1 >> s2;
     if (s1.length()>s2.length()) cout << s1 << ' ' << s1.length();
-    else cout << s2 << ' ' << s2.length();
-    
+    else if (s1.length()<s2.length()) cout << s2 << ' ' << s2.length();
+    else cout << "same";
+
     return 0;
 }
