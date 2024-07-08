eb5f5125fb02de7e78b07d4b7b4cc7b21dbde773
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-07-08 15:38:53 +0900

    Title: 격자 모양 출력하기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240708/격자 모양 출력하기/print-grid-shape.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int n,m,r,c,arr[20][20];
+
+
+int main() {
+
+    cin >> n >> m;
+    for (int i = 0; i<m; i++){
+        cin >> r >> c;
+        arr[r-1][c-1]=r*c;
+    }
+
+    for (int i = 0; i<n; i++){
+        for (int j = 0; j<n; j++){
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
