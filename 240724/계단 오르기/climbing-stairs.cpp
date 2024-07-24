dc0157ae91b1c378e025d6bbcf4d2a7e6f16ca6c
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-07-24 16:15:09 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240724/계단 오르기/climbing-stairs.cpp
+++ 240724/계단 오르기/climbing-stairs.cpp
@@ -7,9 +7,12 @@ int main() {
 
     cin >> n;
 
+    dp[0]=1;
     for (int i = 0 ; i<n; i++){
-        dp[i+2]++;
-        dp[i+3]++;
+        if (dp[i]){
+            dp[i+2]++;
+            dp[i+3]++;
+        }       
     }
     cout << dp[n]%10007;
     return 0;
