bd13bbc42176c4a9d1dc36053bacb06997638979
nunomi0 <109198538+nunomi0@users.noreply.github.com>
2024-07-27 01:29:41 +0900

    Title: 서로 다른 BST 개수 세기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240727/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
+++ 240727/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
@@ -3,20 +3,14 @@ using namespace std;
 
 int n, dp[20];
 
-int cal(int t){
-    if (dp[t]!=-1) return dp[t];
-    if (t<=1) return 1;
-    int cnt=0;
-    for (int i = 0 ; i<n; i++){
-        cnt+=cal(i)*cal(n-i-1);
-    }
-    return dp[t]=cnt;
-}
-
 int main() {
-
     cin >> n;
-    for (int i = 0; i<20; i++) dp[i]=-1;
-    cout << cal(n);
-    return 0;
+    dp[0]=1;
+    dp[1]=1;
+    for (int i = 2; i<=n; i++){
+        int cnt=0;
+        for (int j = 0; j<i; j++) cnt+=dp[j]*dp[n-j-1];
+        dp[i]=cnt;
+    }
+    cout << dp[n];
 }
